978a8d9ef8218e7bd6faec02ad6b01ccc7aa19b5
Joyce <joycesantos8900@gmail.com>
2022-08-18 11:47:01 -0300

    Função diz_ola

---
+++ funções2.py
@@ -0,0 +1,27 @@
+# nome = "Antonio"
+# idade = 50
+# emprego = "Desenvolvedor FrontEnd"
+
+# print (f"Olá meu nome é {nome}, tenho {idade} anos e trabalho como {emprego}.")
+
+# nome = "Joana"
+# idade = 40
+# emprego = "Desenvolvedora BackEnd"
+
+# print (f"Olá meu nome é {nome}, tenho {idade} anos e trabalho como {emprego}.")
+
+# nome = "Carlos"
+# idade = 22
+# emprego = "Estagiario de Desenvolvimento"
+
+# print (f"Olá meu nome é {nome}, tenho {idade} anos e trabalho como {emprego}.")
+
+
+
+def diz_ola(nome,idade,emprego):
+    print (f"Olá meu nome é {nome}, tenho {idade} anos e trabalho como {emprego}.")
+
+
+diz_ola("Antonio",50,"Desenvolvedor FrontEnd")
+diz_ola("Joana",40,"BrackEnd")
+diz_ola("Carlos",22,"Estagiario de Desenvolvimento")
