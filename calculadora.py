79b5d2e43f81373765adc6c84713dff62652fdef
Joyce <joycesantos8900@gmail.com>
2022-08-19 18:56:13 -0300

    alteração de bug, durante teste em calculadora_funções.py

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,6 @@
-numeros = float(input(print("Digite o primeiro número:")))
+numero1 = float(input(print("Digite o primeiro número:")))
 operacao = input(print("Digite a operação"))
 numero2 = float(input(print("Digite o segundo número:")))
-equacao = f"{numeros} {operacao} {numero2}"
+equacao = f"{numero1} {operacao} {numero2}"
 resultado = eval(equacao)
 print(f"{' ' * 20 }\nResultado:{resultado}  {' '*20}\n")
