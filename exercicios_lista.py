10d96f0590721e3679704acfa39e17b61371bed7
Joyce <joycesantos8900@gmail.com>
2022-08-25 18:47:25 -0300

    exercicio de lista

---
+++ exercicios_lista.py
@@ -0,0 +1,20 @@
+#Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
+
+# receber = [1,2,3,4,5]
+# print (receber)
+
+#Faça um Programa que leia um vetor de 10 números reais e mostre-os na ordem inversa.
+
+# receber1 = [1,2,3,4,5,6,7,8,9,0]
+# receber1.reverse()
+# print (receber1)
+
+#Faça um Programa que leia 4 notas, mostre as notas e a média na tela.
+# notas = [10,6,4,7]
+# soma =0
+# for nota in notas:
+#     print (nota)
+#     soma+=nota
+#     calcular =soma/(notas.__len__())
+# print (f"Média: {calcular}")
+
