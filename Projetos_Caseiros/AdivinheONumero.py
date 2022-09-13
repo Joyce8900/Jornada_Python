8849389353bde1c8dc70c4b92c6838748693d2e1
Joyce Santos <92751739+Joyce8900@users.noreply.github.com>
2022-09-13 15:15:02 +0000

    Ajuste no programa adivinhe o número.

--- Projetos_Caseiros/AdivinheONumero.py
+++ Projetos_Caseiros/AdivinheONumero.py
@@ -1,11 +1,18 @@
+
 from random import randint
-numero = randint (10,101)
+numero = randint (1,101)
 print ("Bem vindo ao Jogo de Adivinhe o número!")
 n = int(input("Digite o numero:"))
-while (numero != n):
+contado = 900
+while (numero != n and contado<1000):
   if n > numero:
-    n = int(input(f"Seu número é maior. Digite outro número:"))
+    n = int(input(f"Seu número é maior. Digite um número menor que {n}  :"))
+    contado = contado+100
   elif n<numero:
-    n = int(input(f"Seu número é menor. Digite outro número:"))
-print (numero)
+    n = int(input(f"Seu número é menor. Digite um número maior que {n}  :"))
+    contado=contado+100
+if (contado==1000):
+  print(f"Você já usou todas as suas 10 fichas, o número é {numero}")
+else:
+  print (f"Parabéns você acertou! {numero}")
     
