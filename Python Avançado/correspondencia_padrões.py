a1e0ff374a42e981ea5cab9077e0aa10c9ff1092
Joyce <joycesantos8900@gmail.com>
2022-11-01 14:08:35 -0300

    .

---
+++ Python Avançado/correspondencia_padrões.py
@@ -0,0 +1,18 @@
+#Padrões literais
+
+# genero = "f"
+# match genero:
+#     case "f":
+#         print ("Genero Feminino")
+#     case "m":
+#         print("Genero Masculino")
+#     case _:
+#         print ("Genero não reconhecido")
+
+# idade  =14
+
+# match idade:
+#     case x if x < 18:
+#         print (f"Menor de idade")
+
+#Sequencia
